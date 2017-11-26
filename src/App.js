57115cd292c08da451bdc98b8d8e4add6c101c3b
pjo901018 <pjo901018@korea.ac.kr>
2017-11-27 08:26:22 +0900

    Adding ListGroupItems

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
 import {Grid, Jumbotron} from "react-bootstrap";
 import SearchForm from "./component/SearchForm";
+import Results from "./component/Results";
 
 class App extends Component {
     render() {
         return (
             <div>
-              <Jumbotron>
-                <Grid>
-                  <h1>Search App</h1>
-                  <p>This is a simple search app</p>
+                <Jumbotron>
+                    <Grid>
+                        <h1>Search App</h1>
+                        <p>This is a simple search app</p>
 
-                  <SearchForm />
-                </Grid>
-              </Jumbotron>
+                        <SearchForm />
+                    </Grid>
+                </Jumbotron>
+                <Results />
             </div>
         );
     }
